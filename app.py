ee4af776add455a27165961d2b82c2a71107adde
maxx1404 <maxsonmathew56@gmail.com>
2025-06-23 16:18:52 +0530

    Add Analyze Resume button to Streamlit UI

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from matcher import extract_text_from_resume, compute_similarity, missing_keywor
 st.set_page_config(page_title = "Resume Matcher", layout = "centered")
 st.title("Resume Matcher")
 
-uploaded_file = st.file_uploader("Upload your resume", type="pdf")
+uploaded_file = st.file_uploader("Upload your resume(PDF)", type="pdf")
 jd_input = st.text_area("Paste your Job Description here")
 
 
 if uploaded_file and jd_input:
-    with st.spinner("Analysizing your resume..."):
-        resume_text = extract_text_from_resume(uploaded_file)
-        score = compute_similarity(resume_text, jd_input)
-        missing = missing_keywords(resume_text, jd_input)
-    
-    st.success("Match Completed")
-    st.markdown(f' Match SCORE {score:.2f}')
+    if st.button("Analyze Resume"):
+        with st.spinner("Analysizing your resume..."):
+            resume_text = extract_text_from_resume(uploaded_file)
+            score = compute_similarity(resume_text, jd_input)
+            missing = missing_keywords(resume_text, jd_input)
+        
+        st.success("Match Completed")
+        st.markdown(f' Match SCORE {score:.2f}')
+        if score >= 0.7:
+            st.success("Strong Match")
+        elif score >=0.4:
+            st.success("Decent match")
+        else:
+            st.success("Poor Match")
 
-    st.markdown("Top 5 missing keywrds from youre resume")
-    st.code(", ".join(missing), language='markdown')
+        st.markdown("Top 5 missing keywrds from youre resume")
+        st.code(", ".join(missing), language='markdown')
+
+        
